a280f8e68e81551ca5649208fe1ed8dad4dd6c0e
Daniel Hafezian <danielhafezian@gmail.com>
2025-02-26 23:14:24 +0000

    ficed github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Base path for GitHub Pages user site
+  base: '/danielhafezi.github.io/', // Base path for GitHub Pages repository
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
